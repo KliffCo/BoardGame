0f6642887153f8831910bae345b60da1dcd94f2b
Clifford Corbin <kliffdev@gmail.com>
2025-08-21 20:26:55 +0100

    2025-08-21-0826 PM

--- source/chars/char_manager.gd
+++ source/chars/char_manager.gd
@@ -3,7 +3,9 @@ extends Node3D
 
 static var main: CharManager = null
 @export_file("*.tscn") var _prefab_file: String
+@export_file("*.tres") var _material_file: String
 @onready var _prefab: Resource = load(_prefab_file)
+@onready var _material: Resource = load(_material_file)
 
 var chars: Array[Char] = []
 
@@ -23,3 +25,6 @@ func new_char(data: CharData, slot: RoomCharSlot) -> Char:
 	chr.init(self, count(), data, slot);
 	chars.append(chr)
 	return chr
+
+func new_material() -> ShaderMaterial:
+	return _material.duplicate()
--- source/chars/char_mesh.gd
+++ source/chars/char_mesh.gd
@@ -3,8 +3,8 @@ extends Node3D
 
 var _shadow_mesh: MeshInstance3D
 var _stand_mesh: MeshInstance3D
-var _shadow_mat: StandardMaterial3D
-var _stand_mat#: StandardMaterial3D
+var _shadow_mat: Material
+var _stand_mat: ShaderMaterial
 
 #var _char: Char:
 	#get: return get_parent() as Char
@@ -16,16 +16,21 @@ func _ready() -> void:
 	_shadow_mesh.position = Vector3(0, MeshGen.SPACING*1, 0)
 	#_stand_mat = MeshGen.new_material(_stand_mesh)
 	#var a = load("res://shaders/char_material.tres")
-	_stand_mat = load("res://shaders/char_material.tres")
-	_shadow_mat = MeshGen.new_material(_shadow_mesh)
+	#_stand_mat = load("res://shaders/char_material.tres")
+	_stand_mat = CharManager.main.new_material()
+	_stand_mesh.material_override = _stand_mat
+	#_shadow_mat = CharManager.main.new_material()
 	#_stand_mat.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA_SCISSOR
+	_shadow_mat = MeshGen.new_material(_shadow_mesh)
 	_shadow_mat.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA
 	#_stand_mesh.rotate_x(_stand_mesh.rotation.x + deg_to_rad(-35))
 	_stand_mesh.rotate_x(deg_to_rad(-35))
 
 func set_stand_texture(tex: Texture2D):
+	_stand_mat.set_shader_parameter("tex_albedo", tex)
 	pass
 	#_stand_mat.albedo_texture = tex
 
 func set_shadow_texture(tex: Texture2D):
 	_shadow_mat.albedo_texture = tex
+	#_shadow_mat.albedo_texture = tex
